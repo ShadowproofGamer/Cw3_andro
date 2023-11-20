4ecbddc585cffa8f7ac2143337acc0b367246b98
Shadowproof <kubac400@gmail.com>
2023-11-20 23:12:27 +0100

    Wszystko działa (no powiedzmy)

--- app/src/main/java/com/example/cw3_andro/FragmentRight.kt
+++ app/src/main/java/com/example/cw3_andro/FragmentRight.kt
@@ -18,7 +18,13 @@ import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cw3_andro.databinding.FragmentRightBinding
 import java.lang.reflect.Array
 import java.util.Calendar
 
@@ -37,7 +43,86 @@ class FragmentRight : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    //RF vars
+    private var myAM: ActionMode? = null
+    lateinit var conColor1: TextView
+    lateinit var conPref1: TextView
+    lateinit var conBirth: TextView
+    lateinit var conBirthDate: TextView
 
+
+    //for right activity
+    fun applyBGColor() {
+        val data: SharedPreferences = requireActivity().getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        var colorNum = data.getInt("bg_color", Color.WHITE)
+        conColor1.setBackgroundColor(colorNum)
+    }
+
+    fun setPrefColor(colorNum: Int) {
+        val data: SharedPreferences = requireActivity().getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        val editor = data.edit()
+        editor.putInt("bg_color", colorNum)
+        editor.apply()
+    }
+
+    fun setPrefDate(dateText: String) {
+        val data: SharedPreferences = requireActivity().getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        val editor = data.edit()
+        editor.putString("con_date", dateText)
+        editor.apply()
+    }
+    fun applyDateText() {
+        val data: SharedPreferences = requireActivity().getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        var date = data.getString("con_date", null)
+        conBirthDate.text = date
+    }
+
+    val myAMCallback: ActionMode.Callback = object: ActionMode.Callback {
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            requireActivity().menuInflater.inflate(R.menu.cam_view, menu)
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            //nic nie robimy
+            return true
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            myAM = null
+
+        }
+
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when(item.itemId){
+                R.id.con_act1 -> {
+                    //val color = resources.getColor(R.color.md_theme_red_light_onPrimary, theme)
+                    setPrefColor(Color.RED)
+                    conColor1.setBackgroundColor(Color.RED)
+                    mode.finish()
+                    true
+                }
+                R.id.con_act2 -> {
+                    //val color = resources.getColor(R.color.md_theme_green_light_onPrimary, theme)
+                    setPrefColor(Color.GREEN)
+                    conColor1.setBackgroundColor(Color.GREEN)
+                    mode.finish()
+                    true
+                }
+                R.id.con_act3 -> {
+                    //val color = resources.getColor(R.color.md_theme_grey_light_onPrimary, theme)
+                    setPrefColor(Color.parseColor("#745B00"))
+                    conColor1.setBackgroundColor(Color.parseColor("#745B00"))
+                    mode.finish()
+                    true
+                }
+                else -> {
+                    mode.finish()
+                    false
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +130,7 @@ class FragmentRight : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -55,6 +141,131 @@ class FragmentRight : Fragment() {
         return inflater.inflate(R.layout.fragment_right, container, false)
 
 
+
+    }
+/*
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentRightBinding.inflate(layoutInflater)
+        requireActivity().setContentView(binding.root)
+
+
+        conColor1 = binding.conColor1
+        conPref1 = binding.conPref1
+
+        conColor1.setOnLongClickListener(View.OnLongClickListener { view ->
+            if (myAM != null) {
+                return@OnLongClickListener false
+            }
+            val toolbar = (requireActivity().findViewById<View>(R.id.toolbar1) as androidx.appcompat.widget.Toolbar)
+            myAM = toolbar.startActionMode(myAMCallback)
+            view.isSelected = true
+            true
+        })
+
+        applyBGColor()
+
+        conBirth = binding.conBr
+        conBirthDate = binding.conBrDate
+        applyDateText()
+        conBirthDate.setOnClickListener { _ ->
+            val cal = Calendar.getInstance()
+            val year = cal.get(Calendar.YEAR)
+            val month = cal.get(Calendar.MONTH)
+            val day = cal.get(Calendar.DAY_OF_MONTH)
+            val dateDialog = DatePickerDialog(
+                requireActivity(),
+                {view, year, monthOfYear, dayOfMonth ->
+                    val currCal = Calendar.getInstance()
+                    currCal.set(year, monthOfYear, dayOfMonth)
+                    var tempDate = (dayOfMonth.toString() + "-" + (monthOfYear+1) + "-" + year)
+                    conBirthDate.text = tempDate
+                    setPrefDate(tempDate)
+                },
+                year,
+                month,
+                day
+            ).show()
+        }
+
+        val backButton: Button? = binding.backButton
+        backButton!!.setOnClickListener { _ ->
+            val builder: AlertDialog.Builder = AlertDialog.Builder(requireActivity())
+            builder
+                .setTitle("Go Back Dialog")
+                .setMessage("Are you sure at 100% ?")
+                //.setSingleChoiceItems()
+                .setPositiveButton("Accept") { dialog, which ->
+                    requireActivity().onBackPressed()
+                }.setNegativeButton("Cancel") { dialog, which ->
+                    dialog.cancel()
+                }.create().show()
+        }
+
+    }
+
+ */
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val binding = FragmentRightBinding.inflate(layoutInflater)
+        //requireActivity().setContentView(binding.root)
+
+
+        conColor1 = (requireActivity().findViewById<View>(R.id.con_color1) as TextView)
+        conPref1 = (requireActivity().findViewById<View>(R.id.con_pref1) as TextView)
+
+        conColor1.setOnLongClickListener(View.OnLongClickListener { view ->
+            if (myAM != null) {
+                return@OnLongClickListener false
+            }
+            val toolbar = (requireActivity().findViewById<View>(R.id.toolbar1) as androidx.appcompat.widget.Toolbar)
+            myAM = toolbar.startActionMode(myAMCallback)
+            view.isSelected = true
+            true
+        })
+
+        applyBGColor()
+
+        conBirth = (requireActivity().findViewById<View>(R.id.con_br) as TextView)
+        conBirthDate = (requireActivity().findViewById<View>(R.id.con_br_date) as TextView)
+        applyDateText()
+        conBirthDate.setOnClickListener { _ ->
+            val cal = Calendar.getInstance()
+            val year = cal.get(Calendar.YEAR)
+            val month = cal.get(Calendar.MONTH)
+            val day = cal.get(Calendar.DAY_OF_MONTH)
+            val dateDialog = DatePickerDialog(
+                requireActivity(),
+                {view, year, monthOfYear, dayOfMonth ->
+                    val currCal = Calendar.getInstance()
+                    currCal.set(year, monthOfYear, dayOfMonth)
+                    var tempDate = (dayOfMonth.toString() + "-" + (monthOfYear+1) + "-" + year)
+                    conBirthDate.text = tempDate
+                    setPrefDate(tempDate)
+                },
+                year,
+                month,
+                day
+            ).show()
+        }
+
+        val backButton: Button? = (requireActivity().findViewById<View>(R.id.back_button) as Button)
+        backButton!!.setOnClickListener { _ ->
+            val builder: AlertDialog.Builder = AlertDialog.Builder(requireActivity())
+            builder
+                .setTitle("Go Back Dialog")
+                .setMessage("Are you sure at 100% ?")
+                //.setSingleChoiceItems()
+                .setPositiveButton("Accept") { dialog, which ->
+                    requireActivity().onBackPressed()
+                }.setNegativeButton("Cancel") { dialog, which ->
+                    dialog.cancel()
+                }.create().show()
+        }
 
     }
 
